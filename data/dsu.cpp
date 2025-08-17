34490a6ef6ef209ccc15ea48a1ec9c0c39312362
notbraining <brian.tianxiao.zhao@gmail.com>
2025-08-17 14:29:20 -0700

    made dsu to be struct, used sz instead of only 1 par vector

--- data/dsu.cpp
+++ data/dsu.cpp
@@ -1,13 +1,24 @@
-vector<int> par;
-int n;
-int get(int x) { return (par[x] < 0 ? x : (par[x] = get(par[x]))); }
-void merge(int a, int b) {
-  a = get(a);
-  b = get(b);
-  if (a == b) return;
-  if (par[a] < par[b]) {
-    swap(a, b);
-  }
-  par[b] += par[a];
-  par[a] = b;
-}
+struct DSU{
+    vector<int> par;
+    vector<int>sz;
+    DSU(int _n){
+        par = vector<int>(_n + 1);
+        sz = vector<int>(_n + 1, 1);
+        for(int i = 0; i <= _n; ++i){
+            par[i] = i;
+        }
+    }
+    //with path compression
+    int get(int x) { return (par[x] == x ? x : (par[x] = get(par[x]))); }
+    //small to large merging
+    void merge(int a, int b) {
+        a = get(a);
+        b = get(b);
+        if(a == b) return;
+        if(sz[a] > sz[b]) {
+            swap(a, b);
+        }
+        sz[b] += sz[a];
+        par[a] = b;
+    }
+};
