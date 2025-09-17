c68ba4ec05742cc00e3931e256496231a70370c6
Brian Zhao <brianzhao@Brians-Air.lan>
2025-09-16 20:40:17 -0700

    added stress testing

---
+++ stress/gen.cpp
@@ -0,0 +1,28 @@
+#pragma GCC optimize("O3")
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+#define int long long
+#define pii pair<int, int>
+#define LL long long
+int rnd(int a, int b) { return a + rand() % (b - a + 1); }
+int32_t main(int argc, char* argv[]) {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int seed = atoi(argv[1]);
+  srand(seed);
+}
