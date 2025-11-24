c4c7140c8680e4e4de34d41685866663d5a88985
notbraining <brian.tianxiao.zhao@gmail.com>
2025-11-23 22:26:34 -0800

    Update and rename fastpow.cpp to fastpow_and_combo.cpp

--- math/fastpow_and_combo.cpp
+++ math/fastpow_and_combo.cpp
@@ -12,3 +12,9 @@ int fastpow(int base, int power){
         return (f) * (f) % MOD * base % MOD;
     }
 }
+	choose[0][0] = 1;
+	for (i = 1; i <= n / 2; ++i) {
+		choose[i][0] = 1;
+		for (int j = 1; j <= i; ++j)
+			choose[i][j] = choose[i - 1][j]+choose[i - 1][j - 1]);
+	}
