9fa77f08c5a16062a8a121b30c42eb75d0683e17
notbraining <brian.tianxiao.zhao@gmail.com>
2025-12-20 16:17:44 -0800

    Refactor query method and add range check
    Fix query method to handle invalid ranges and rename it to prefix_query.

--- data/fenwick.cpp
+++ data/fenwick.cpp
@@ -2,26 +2,31 @@ struct fenwick{
     vector<int>v;
     int size;
     fenwick(int sz){
+        sz+=10;
         size = sz;
         v = vector<int>(sz + 1);
     }
 
 
     int query(int l, int r){
-        return (query(r) - query(l - 1)+MOD)%MOD;
+        if(l>r)
+            return 0;
+        return (prefix_query(r) - prefix_query(l - 1)+MOD)%MOD;
     }
     void update(int pos, int val){
         while(pos < v.size()){
             v[pos] += val;
+            v[pos]%=MOD;
             //v[pos] %= MOD;
             pos += pos & (-pos);
         }
     }
 //TIP: update gets INF loop if pos = 0, make sure all inputs are between 1 and n
-    int query(int pos){
+    int prefix_query(int pos){
         int ans = 0;
         while(pos > 0){
             ans += v[pos];
+            ans%=MOD;
             //ans %= MOD;
             pos -= pos & (-pos);
         }
