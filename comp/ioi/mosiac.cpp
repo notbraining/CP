802e81f39b697f4bc531188198c58c28cb43f251
notbraining <brian.tianxiao.zhao@gmail.com>
2026-06-20 11:44:03 -0700

    original impl

--- comp/ioi/mosiac.cpp
+++ comp/ioi/mosiac.cpp
@@ -59,11 +59,15 @@ vector<long long> mosaic(
             psum[i][j] = (i ? psum[i - 1][j] : 0) + (j ? psum[i][j - 1] : 0) - (i && j ? psum[i - 1][j - 1] : 0) + v[i][j];
         }
     }
+
+
     auto solve_rect = [&](int t, int b, int l, int r){
         return psum[b][r] - (l ? psum[b][l - 1] : 0) - (t ? psum[t - 1][r] : 0) + ((t && l) ? psum[t - 1][l - 1] : 0);
         };
 
     vector<ll>sum1(n - M), sum2(n - M), sum3(n - M), sum4(n - M);
+
+
     for(int i = M; i < n; ++i){
         sum1[i - M] = ((i - M) ? sum1[i - M - 1] : 0) + v[i - 1][M - 1];
         sum3[i - M] = ((i - M) ? sum3[i - M - 1] : 0) + v[i - 1][M - 1] * (n - i);
@@ -101,12 +105,22 @@ vector<long long> mosaic(
         if(b < 0 || r < 0)return 0LL;
         ll ans = 0;
         ll peak = min(b, r) + 1;
+
+        //cout << "add: " << sum3[b] - ((b - peak >= 0) ? sum3[b - peak] : 0) << endl;
+        //cout << "subtract: " << ((sum1[b] - ((b - peak >= 0) ? sum1[b - peak] : 0)) * (n - M - b - 1)) << endl;
         ans += sum3[b] - ((b - peak >= 0) ? sum3[b - peak] : 0) - ((sum1[b] - ((b - peak >= 0) ? sum1[b - peak] : 0)) * (n - M - b - 1));
         ans += sum4[r] - ((r - peak >= 0) ? sum4[r - peak] : 0) - ((sum2[r] - ((r - peak >= 0) ? sum2[r - peak] : 0)) * (n - M - r - 1));
-        if(r > b)
+
+        //cout << "sum4: " << sum4[r] - ((r - peak >= 0) ? sum4[r - peak] : 0) << endl;
+
+        //cout << "curans: " << ans << endl;
+        if(r > b){
+            //have some more sum2
             ans += sum2[r - b - 1] * (peak);
-        if(b > r)
+        }
+        if(b > r){
             ans += sum1[b - r - 1] * (peak);
+        }
         ans -= v[M - 1][M - 1] * peak;
         return ans;
         } ;
@@ -119,93 +133,31 @@ vector<long long> mosaic(
         if(b < M){
             ret[i] = solve_rect(t, b, l, r); continue;
         }
+
+
         if(l < M && t < M){
             ret[i] = solve_rect(t, b, l, M - 1) ;
             ret[i] += solve_rect(t, min(M - 1, b), M, r);
         }
+
+
         if(l < M && t >= M){
             ret[i] += solve_rect(t, b, l, M - 1);
         }
         if(l >= M && t < M){
+            // cout << "LL";
             ret[i] += solve_rect(t, M - 1, l, r);
         }
 
+
+
+        // cout << "curr_ret: " << ret[i] << endl;
         if(b < M)continue;
+        // cout << b - M << " " << r - M << endl;
+        //cout << hardwork(b - M, r - M) << endl;
 
         ret[i] += hardwork(b - M, r - M) - hardwork(t - 1 - M, r - M) - hardwork(b - M, l - 1 - M) + hardwork(t - 1 - M, l - 1 - M);
 
     }
     return ret;
-}
-
-
-
-
-
-
-//technically the end of program
-
-
-
-
-
-
-
-
-
-
-
-vector<long long> mosaic2(
-    vector<int> X, vector<int> Y,
-    vector<int> T, vector<int> B,
-    vector<int> L, vector<int> R){
-    int n = X.size(); int q = T.size();
-
-    vector<vector<ll>>v(n, vector<ll>(n));
-    for(int i = 0; i < n; ++i){
-        v[0][i] = X[i];
-        v[i][0] = Y[i];
-    }
-    for(int i = 1; i < n; ++i){
-        for(int j = 1; j < n; ++j){
-            if(v[i - 1][j] + v[i][j - 1] == 0){
-                v[i][j] = 1;
-            } else{
-                v[i][j] = 0;
-            }
-        }
-    }
-    vector <ll>ret(q);
-    for(int i = 0; i < q; ++i){
-        int ans = 0;
-        for(int j = T[i]; j <= B[i]; ++j){
-            for(int k = L[i]; k <= R[i]; ++k){
-                ans += v[j][k];
-            }
-        }
-        ret[i] = ans;
-    }
-
-    return ret;
-
-}
-int32_t main(){
-    cin.tie(0)->sync_with_stdio(0);
-    vector<int>x = {1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0};
-    vector<int>y = {1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0};
-    vector<int>t = {2};
-    vector<int>b = {8};
-    vector<int>l = {3};
-    vector<int>r = {4};
-
-    auto ret = mosaic(x, y, t, b, l, r);
-    auto ret2 = mosaic2(x, y, t, b, l, r);
-    // assert(ret == ret2);
-    for(int i : ret){
-        cout << i << endl;
-    }
-    for(int i : ret2){
-        cout << i << endl;
-    }
-
 }
